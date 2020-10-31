b75d63f6c923156c563b9b0f65042ecd03823d6f
mdyounusali <30723679+mdyounusali@users.noreply.github.com>
2020-10-31 12:32:43 +0600

    Qup

--- autoComment.py
+++ autoComment.py
@@ -30,9 +30,9 @@ def name():
 
 
 while True:      #forever loop
-    pyautogui.typewrite("I love you {name()}")  #type message
+    pyautogui.typewrite('I love you {name()}')  #type message
     sleep(.600)                        #A bit delay of 600 ms
-    pyautogui.typewrite("\n")          #New line, here 'Enter' to send text
+    pyautogui.typewrite('enter')          #New line, here 'Enter' to send text
     sleep(2)                           #delay of 2 seconds
 
     x = x - 1         #decrement x value by 1
